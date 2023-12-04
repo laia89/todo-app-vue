5c7f772f5bdaf8731e8bed912d519c8357f313f4
Laia <laiacy234@gmail.com>
2023-12-04 13:22:14 +0100

    Pass input text into the addTodo function

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,38 @@
 import { ref } from 'vue'
 
 const todos = ref([])
+const text = ref('')
+
+function addTodo() {
+  if (text.value.trim() === '') {
+    return
+  }
+
+  const todo = {
+    todo: text.value,
+    completed: false
+  }
+
+  todos.value.unshift(todo)
+  text.value = ''
+}
 </script>
+
+<template>
+  <header class="app">
+    <section class="greeting">
+      <h3 class="title">ToDo Application</h3>
+    </section>
+    <div class="input-section">
+      <section class="create-todo">
+        <form @submit.prevent="addTodo">
+          <h3>What is your main focus for today?</h3>
+          <input type="text" placeholder="e.g. Pet your cat" v-model="text" />
+          <input type="submit" value="Add todo" />
+        </form>
+      </section>
+    </div>
+  </header>
+
+  <main class="app"></main>
+</template>

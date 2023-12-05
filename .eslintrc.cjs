ced77728f2dcd9f7dcea449c1f1e86d649713acf
Laia <laiacy234@gmail.com>
2023-12-05 10:07:37 +0100

    Use Vitest for testing and coverage

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,20 +3,19 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      files: ['*.test.js'],
+      env: {
+        jest: true
+      },
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended']
     }
   ],
   parserOptions: {
